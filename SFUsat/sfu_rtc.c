b39e37dd04d3f4bc743b8a18f01fc940b113e06b
Steven Huang <stevenhuang1995@gmail.com>
2017-06-13 03:09:59 -0700

    Preliminary Real Time Clock (RTC) plumbing

---
+++ SFUsat/sfu_rtc.c
@@ -0,0 +1,38 @@
+/*
+ * sfu_rtc.c
+ *
+ *  Created on: Jun 12, 2017
+ *      Author: steven
+ */
+
+#include "sfu_rtc.h"
+
+unsigned int rtc_time = 0;
+unsigned int het_time = 0;
+
+unsigned int getTime() {
+	/*
+	 * Get current HET time.
+	 */
+	het_time = 0;
+
+	/*
+	 * Return "approximation" to actual RTC time.
+	 */
+	return rtc_time + het_time;
+}
+
+unsigned int getRTCTime() {
+	/*
+	 * Reset HET time.
+	 */
+	het_time = 0;
+
+	/*
+	 * Initiate RTC SPI transfer.
+	 */
+	rtc_time = 0;
+
+	return rtc_time;
+}
+
---
+++ SFUsat/sfu_rtc.h
@@ -0,0 +1,26 @@
+/*
+ * sfu_rtc.h
+ *
+ *  Created on: Jun 12, 2017
+ *      Author: steven
+ */
+
+#ifndef SFUSAT_SFU_RTC_H_
+#define SFUSAT_SFU_RTC_H_
+
+/**
+ * Get "approximated" RTC epoch time.
+ * Combines last actual time from RTC with time from Hercule's HET.
+ * @return seconds
+ */
+unsigned int getTime();
+
+/**
+ * Get RTC epoch time.
+ * RTC time is treated as ground truth.
+ * Initiates a SPI transaction.
+ * @return
+ */
+unsigned int getRTCTime();
+
+#endif /* SFUSAT_SFU_RTC_H_ */
