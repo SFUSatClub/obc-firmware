98e73e43750d37ddc8961c65b9e1dd3ec1dd8ed1
Steven Huang <stevenhuang1995@gmail.com>
2017-06-13 03:08:51 -0700

    Sort events array on target time #5

--- SFUsat/sfu_scheduler.c
+++ SFUsat/sfu_scheduler.c
@@ -7,29 +7,41 @@
 
 #include "sfu_scheduler.h"
 #include "sfu_uart.h"
+#include "sfu_rtc.h"
+
+
+void shift(int idx, int dir);
 
 int addEvent(Event_t event) {
 	if (schedule.numActiveEvents >= MAX_EVENTS) {
 		return 0;
 	}
-	schedule.numActiveEvents++;
+	event.target_time = getTime() + event.seconds_from_now;
 	event._status.active = 1;
 	int i;
 	for (i = 0; i < MAX_EVENTS; i++) {
-		if (schedule.events[i]._status.active == 0) {
+		if (schedule.events[i]._status.active) {
+			if (event.target_time < schedule.events[i].target_time) {
+				shift(i, 1);
+				schedule.events[i] = event;
+				break;
+			}
+		} else {
 			schedule.events[i] = event;
 			break;
 		}
 	}
+	schedule.numActiveEvents++;
 	return 1;
 }
 
-int removeEvent(int eventIdx) {
+int removeEventIdx(int eventIdx) {
 	if (eventIdx < 0 || eventIdx >= MAX_EVENTS || !schedule.events[eventIdx]._status.active) {
 		return 0;
 	}
-	schedule.numActiveEvents--;
 	schedule.events[eventIdx]._status.active = 0;
+	shift(eventIdx + 1, -1);
+	schedule.numActiveEvents--;
 	return 1;
 }
 
@@ -50,3 +62,31 @@ Schedule_t schedule = {
 		.events = {0},
 };
 
+/**
+ * Shifts all elements from range idx to boundary a certain amount dir.
+ * If dir is -1, shifts left by 1, dir is 3, shifts right 3, etc.
+ * Elements in original holes will be set inactive after shift operation.
+ * Shifts preserve ordering (active elements precede inactive elements).
+ * @param idx left bound inclusive
+ * @param dir amount to shift
+ */
+void shift(int idx, int dir) {
+	/*
+	 * Boundary is the index of the rightmost active event in the array.
+	 */
+	const int boundary = schedule.numActiveEvents - 1;
+	/*
+	 * If shifting left, we need to start at the left boundary, stop at right boundary, and increment i.
+	 * If shifting right, we need to start at the right boundary, stop at left boundary, and decrement i.
+	 * Otherwise, we will overwrite what we want to shift.
+	 */
+	int i;
+	// TODO: make this prettier...
+	for (i = dir < 0 ? idx : boundary; i <= dir < 0 ? boundary : idx; i = dir < 0 ? i + 1 : i - 1) {
+		const int dest = i + dir;
+		if (dest >= 0 && dest < MAX_EVENTS) {
+			schedule.events[dest] = schedule.events[i];
+		}
+		schedule.events[i]._status.active = 0;
+	}
+}
--- SFUsat/sfu_scheduler.h
+++ SFUsat/sfu_scheduler.h
@@ -15,7 +15,7 @@
  */
 typedef struct EventStatus {
 	/*
-	 * An event is active if its its arguments are valid and it's ready to be invoked.
+	 * An event is active if its arguments are valid and it's ready to be invoked.
 	 * An event is flagged active (set to 1) once it is added to the scheduler.
 	 * An event is flagged inactive (set to 0) after it has been invoked.
 	 */
@@ -31,9 +31,24 @@ typedef struct EventStatus {
  */
 typedef struct Event {
 	/**
-	 * Seconds from now when action should be invoked.
+	 * Only either seconds_from_now or target_time are ever used at the same time, so they are made unions.
+	 * This saves memory, in addition to providing more semantic meaning when conditions change.
+	 *
+	 * seconds_from_now
+	 * 		- Seconds from now when action should be invoked.
+	 * 		- Events are always assumed to be initialized with this variable first.
+	 * 		- This variable loses semantic meaning after initialization (will have value of target_time).
+	 *
+	 * target_time
+	 * 		- Once initialized with seconds_from_now, the scheduler will convert this value to a target time.
+	 * 		  This is done by retrieving the current time from the RTC and adding to it seconds_from_now.
+	 * 		- This variable only has semantic meaning after initialization.
 	 */
-	unsigned int target_time;
+	union {
+		unsigned int seconds_from_now;
+		unsigned int target_time;
+	};
+
 	/**
 	 * The action (command) to invoke once target_time is reached.
 	 */
@@ -52,9 +67,21 @@ typedef struct Event {
 
 /**
  * Keeps track of events.
+ * Events are added/removed rarely relative to how often they are scanned, so
+ * improving the scanning speed will be good for performance.
+ *
+ * This is done by sorting the events array when events are added/removed/fired.
+ *
+ * When the events array is sorted...
+ * 		- all active events precede (have lower indices than) inactive events
+ * 		- all active events are sorted on their target times from least to greatest
  */
 #define MAX_EVENTS 15
 typedef struct Schedule {
+	/*
+	 * Index at (numActiveEvents - 1) demarks the rightmost active event.
+	 * Indices after this point are either inactive events or past array bounds.
+	 */
 	int numActiveEvents;
 	Event_t events[MAX_EVENTS];
 } Schedule_t;
@@ -71,10 +98,17 @@ int addEvent(Event_t event);
 
 /**
  * Removes an event from the schedule.
+ * @param event event to remove
+ * @return 1 if event is removed from the schedule, 0 if failed to remove event.
+ */
+//int removeEvent(Event_t event);
+
+/**
+ * Removes an event from the schedule.
  * @param eventIdx index of event to remove
  * @return 1 if event is removed from the schedule, 0 if failed to remove event.
  */
-int removeEvent(int eventIdx);
+int removeEventIdx(int eventIdx);
 
 void showActiveEvents();
 
