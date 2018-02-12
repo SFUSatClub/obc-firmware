f958bf9025d238c14321d8d2ea4f6a9cd98e3848
Steven Huang <stevenhuang1995@gmail.com>
2018-02-12 14:36:11 -0500

    Consistent build config names and delete+ignore previous+current build dirs

--- OBC v0.3/OBC 0.4/SFUsat/unit_tests/subdir_vars.mk
+++
@@ -1,9 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-# Add inputs and outputs from these tool invocations to the build variables 
-OBJ_SRCS += \
-../OBC\ 0.4/SFUsat/unit_tests/test_adc.obj 
-
-
