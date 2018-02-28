841a5637ee06549431b0ec2b56504ce7d53424f9
Richard Arthurs <rarthurs@sfu.ca>
2018-02-28 08:00:54 -0800

    Fixed sysmem warning

--- platform-obc-v0.3/source/sys_link.cmd
+++ platform-obc-v0.3/source/sys_link.cmd
@@ -84,6 +84,7 @@ SECTIONS
     .kernelHEAP   : {} > RAM
     .bss          : {} > RAM
     .data         : {} > RAM
+    .sysmem  : {} > RAM // RA: added to get rid of warning. If we run into issues, investigate what this does
 
 /* USER CODE BEGIN (4) */
 /* USER CODE END */
