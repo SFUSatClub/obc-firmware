550d7f03a04108233cd1cc1203dc0093204fe0de
Steven Huang <stevenhuang1995@gmail.com>
2018-03-04 05:11:11 -0500

    Enable MIBSPI3 for RF, GIOA_5 for IRQ on platform-obc-v0.3/4
    - Ensure linker fixes in last two commits persist through halcogen source regeneration
    - Use #ifdef PLATFORM_LAUNCHPAD, etc, to switch on project configurations instead of comparing numbers

--- SFUsat/sfu_hardwaredefs.h
+++ SFUsat/sfu_hardwaredefs.h
@@ -14,139 +14,172 @@
 #include "spi.h"
 #include "mibspi.h"
 
-// ======================= SETTINGS FOR v0.4 =======================
-#if SFUSAT_BOARD == 2
-//  ---------------  RTC Defines --------------------
-// Todo: confirm this works. Convert to MIBSPI like for flash section
-#define RTC_CS_PORT spiPORT5
-#define RTC_CS_PIN 0
-#define RTC_SPI_REG spiREG5
-//RTC SPI Config (set in rtcInit())
-#define RTC_CONFIG_CS_HOLD 0 //CS false = high during data transfer
-#define RTC_CONFIG_WDEL 1 // wdelay
-#define RTC_CONFIG_DFSEL SPI_FMT_0 // data format
-#define RTC_CONFIG_CSNR 0x00 // chip select to use
-
-// ---------------- General Functionality ------------------
-#define DEBUG_LED_PORT gioPORTA
-#define DEBUG_LED_PIN 6
-
-#define WATCHDOG_TICKLE_PORT gioPORTA
-#define WATCHDOG_TICKLE_PIN 7
-
-#define DEMO_ADC_REG adcREG1
-#define DEMO_ADC_PIN 1
-
-#define UART_PORT sciREG
-
-// ----------------- Radio -------------------------
-#define TASK_RADIO_REG spiREG3
-
-// ---------------- RF IRQ ------------------
-#define RF_IRQ_PIN 2
-#define RF_IRQ_PORT gioPORTB
-
-//----------------- FLASH --------------------
-#define FLASH_MIBSPI_REG mibspiREG1
-#define FLASH_DATA_FORMAT 0
-#define FLASH_6_BYTE_GROUP 0
-#define FLASH_1_BYTE_GROUP 1 // transfer group with 1 byte length
-#define FLASH_2_BYTE_GROUP 2 // transfer group with 2 byte length
-#define FLASH_4_BYTE_GROUP 4 // transfer group with 4 byte length
-#define FLASH_20_BYTE_GROUP 3 // TG 20 byte length
-#define FLASH_CHIP_TYPE 1 // 0 = SST26, 1 = IS25LP016D
-
-#endif
-
-
-// =======================  SETTINGS FOR v0.3 =======================
-#if SFUSAT_BOARD == 1
-//  ---------------  RTC Defines --------------------
-#define RTC_CS_PORT spiPORT4
-#define RTC_CS_PIN 0
-#define RTC_SPI_REG spiREG4
+/******************************************************************************
+ *
+ * SETTINGS FOR PLATFORM-OBC-V0.4
+ *
+ *****************************************************************************/
+#ifdef PLATFORM_OBC_V0_4
+/**
+ * RTC
+ */
+// TODO: confirm this works. Convert to MIBSPI like for flash section
+#define RTC_CS_PORT 			spiPORT5
+#define RTC_CS_PIN 				0
+#define RTC_SPI_REG 			spiREG5
 // RTC SPI Config (set in rtcInit())
-#define RTC_CONFIG_CS_HOLD 0 //CS false = high during data transfer
-#define RTC_CONFIG_WDEL 1 // wdelay
-#define RTC_CONFIG_DFSEL SPI_FMT_0 // data format
-#define RTC_CONFIG_CSNR 0x00 // chip select to use
+#define RTC_CONFIG_CS_HOLD 		0 			// CS false = high during data transfer
+#define RTC_CONFIG_WDEL 		1 			// wdelay
+#define RTC_CONFIG_DFSEL 		SPI_FMT_0 	// data format
+#define RTC_CONFIG_CSNR 		0x00 		// chip select to use
 
-// ---------------- General Functionality ------------------
-#define DEBUG_LED_PORT gioPORTA
-#define DEBUG_LED_PIN 6
+/**
+ * General Functionality
+ */
+#define DEBUG_LED_PORT			gioPORTA
+#define DEBUG_LED_PIN			6
+#define WATCHDOG_TICKLE_PORT	gioPORTA
+#define WATCHDOG_TICKLE_PIN		7
+#define DEMO_ADC_REG			adcREG1
+#define DEMO_ADC_PIN			1
+#define UART_PORT				sciREG
 
-#define WATCHDOG_TICKLE_PORT gioPORTA
-#define WATCHDOG_TICKLE_PIN 7
+/**
+ * Radio
+ */
+#define TASK_RADIO_REG 			spiREG3
 
-#define DEMO_ADC_REG adcREG1
-#define DEMO_ADC_PIN 2
+/**
+ * IRQs
+ */
+#define RF_IRQ_PIN 				2
+#define RF_IRQ_PORT 			gioPORTB
+#define GIO_IRQ_PIN 			5
+#define GIO_IRQ_PORT 			gioPORTA
 
-#define UART_PORT sciREG
+/**
+ * Flash
+ */
+#define FLASH_MIBSPI_REG 		mibspiREG1
+#define FLASH_DATA_FORMAT 		0
+#define FLASH_6_BYTE_GROUP 		0
+#define FLASH_1_BYTE_GROUP 		1 // transfer group with 1 byte length
+#define FLASH_2_BYTE_GROUP 		2 // transfer group with 2 byte length
+#define FLASH_4_BYTE_GROUP 		4 // transfer group with 4 byte length
+#define FLASH_20_BYTE_GROUP		3 // TG 20 byte length
+#define FLASH_CHIP_TYPE 		1 // 0 = SST26, 1 = IS25LP016D
+#endif /* PLATFORM_OBC_V0_4 */
 
-// ----------------- Radio -------------------------
-#define TASK_RADIO_REG spiREG3
 
-// ---------------- RF IRQ ------------------
-#define RF_IRQ_PIN 2
-#define RF_IRQ_PORT gioPORTB
-
-//----------------- FLASH --------------------
-#define FLASH_MIBSPI_REG mibspiREG1
-#define FLASH_DATA_FORMAT 0
-#define FLASH_6_BYTE_GROUP 0
-#define FLASH_1_BYTE_GROUP 1 // transfer group with 1 byte length
-#define FLASH_2_BYTE_GROUP 2 // transfer group with 2 byte length
-#define FLASH_4_BYTE_GROUP 4 // transfer group with 4 byte length
-#define FLASH_20_BYTE_GROUP 3 // TG 20 byte length
-#define FLASH_CHIP_TYPE 1 // 0 = SST26, 1 = IS25LP016D
-#endif
-
-// ======================= SETTINGS FOR LAUNCHPAD =======================
-#if SFUSAT_BOARD == 0
-//  ---------------  RTC Defines --------------------
-#define RTC_CS_PORT gioPORTA
-#define RTC_CS_PIN 1
-#define RTC_SPI_REG spiREG2
+/******************************************************************************
+ *
+ * SETTINGS FOR PLATFORM-OBC-V0.3
+ *
+ *****************************************************************************/
+#ifdef PLATFORM_OBC_V0_3
+/**
+ * RTC
+ */
+#define RTC_CS_PORT 			spiPORT4
+#define RTC_CS_PIN 				0
+#define RTC_SPI_REG 			spiREG4
 // RTC SPI Config (set in rtcInit())
-#define RTC_CONFIG_CS_HOLD 0 //CS false = high during data transfer
-#define RTC_CONFIG_WDEL 1 // wdelay
-#define RTC_CONFIG_DFSEL SPI_FMT_0 // data format
-#define RTC_CONFIG_CSNR 0x00 // chip select to use
+#define RTC_CONFIG_CS_HOLD 		0 			// CS false = high during data transfer
+#define RTC_CONFIG_WDEL 		1 			// wdelay
+#define RTC_CONFIG_DFSEL 		SPI_FMT_0 	// data format
+#define RTC_CONFIG_CSNR 		0x00 		// chip select to use
 
-// ---------------- General Functionality ------------------
-#define DEBUG_LED_PORT gioPORTA
-#define DEBUG_LED_PIN 2
+/**
+ * General Functionality
+ */
+#define DEBUG_LED_PORT 			gioPORTA
+#define DEBUG_LED_PIN 			6
+#define WATCHDOG_TICKLE_PORT 	gioPORTA
+#define WATCHDOG_TICKLE_PIN 	7
+#define DEMO_ADC_REG 			adcREG1
+#define DEMO_ADC_PIN 			2
+#define UART_PORT 				sciREG
 
-#define WATCHDOG_TICKLE_PORT gioPORTA
-#define WATCHDOG_TICKLE_PIN 7
+/**
+ * Radio
+ */
+#define TASK_RADIO_REG 			spiREG3
 
-#define DEMO_ADC_REG adcREG1
-#define DEMO_ADC_PIN 1
+/**
+ * IRQs
+ */
+#define RF_IRQ_PIN 				2
+#define RF_IRQ_PORT 			gioPORTB
+#define GIO_IRQ_PIN 			5
+#define GIO_IRQ_PORT 			gioPORTA
 
-#define UART_PORT scilinREG
-
-// ----------------- Radio -------------------------
-#define TASK_RADIO_REG spiREG3
-
-// ---------------- RF IRQ ------------------
-#define RF_IRQ_PIN 0
-#define RF_IRQ_PORT gioPORTA
-// ---------------- GIO IRQ ------------------
-#define GIO_IRQ_PIN 7
-#define GIO_IRQ_PORT gioPORTA
-
-//----------------- FLASH --------------------
-#define FLASH_MIBSPI_REG mibspiREG1
-#define FLASH_6_BYTE_GROUP 0
-#define FLASH_1_BYTE_GROUP 1 // transfer group with 1 byte length
-#define FLASH_2_BYTE_GROUP 2 // transfer group with 2 byte length
-#define FLASH_4_BYTE_GROUP 4 // transfer group with 4 byte length
-#define FLASH_20_BYTE_GROUP 3 // TG 20 byte length
-#define FLASH_CHIP_TYPE 0 // 0 = SST26, 1 = IS25LP016D
-
-#endif
+/**
+ * Flash
+ */
+#define FLASH_MIBSPI_REG 		mibspiREG1
+#define FLASH_DATA_FORMAT 		0
+#define FLASH_6_BYTE_GROUP 		0
+#define FLASH_1_BYTE_GROUP 		1 // transfer group with 1 byte length
+#define FLASH_2_BYTE_GROUP 		2 // transfer group with 2 byte length
+#define FLASH_4_BYTE_GROUP 		4 // transfer group with 4 byte length
+#define FLASH_20_BYTE_GROUP 	3 // TG 20 byte length
+#define FLASH_CHIP_TYPE 		1 // 0 = SST26, 1 = IS25LP016D
+#endif /* PLATFORM_OBC_V0_3 */
 
 
+/******************************************************************************
+ *
+ * SETTINGS FOR LAUNCHPAD
+ *
+ *****************************************************************************/
+#ifdef PLATFORM_LAUNCHPAD
+/**
+ * RTC
+ */
+#define RTC_CS_PORT 			gioPORTA
+#define RTC_CS_PIN 				1
+#define RTC_SPI_REG 			spiREG2
+// RTC SPI Config (set in rtcInit())
+#define RTC_CONFIG_CS_HOLD 		0 			// CS false = high during data transfer
+#define RTC_CONFIG_WDEL 		1 			// wdelay
+#define RTC_CONFIG_DFSEL 		SPI_FMT_0 	// data format
+#define RTC_CONFIG_CSNR 		0x00 		// chip select to use
 
-#endif
+/**
+ * General Functionality
+ */
+#define DEBUG_LED_PORT 			gioPORTA
+#define DEBUG_LED_PIN 			2
+#define WATCHDOG_TICKLE_PORT 	gioPORTA
+#define WATCHDOG_TICKLE_PIN 	7
+#define DEMO_ADC_REG 			adcREG1
+#define DEMO_ADC_PIN 			1
+#define UART_PORT 				scilinREG
+
+/**
+ * Radio
+ */
+#define TASK_RADIO_REG 			spiREG3
+
+/**
+ * IRQs
+ */
+#define RF_IRQ_PIN 				0
+#define RF_IRQ_PORT 			gioPORTA
+#define GIO_IRQ_PIN 			7
+#define GIO_IRQ_PORT 			gioPORTA
+
+/**
+ * Flash
+ */
+#define FLASH_MIBSPI_REG 		mibspiREG1
+#define FLASH_6_BYTE_GROUP 		0
+#define FLASH_1_BYTE_GROUP 		1 // transfer group with 1 byte length
+#define FLASH_2_BYTE_GROUP 		2 // transfer group with 2 byte length
+#define FLASH_4_BYTE_GROUP 		4 // transfer group with 4 byte length
+#define FLASH_20_BYTE_GROUP 	3 // TG 20 byte length
+#define FLASH_CHIP_TYPE 		0 // 0 = SST26, 1 = IS25LP016D
+#endif /* PLATFORM_LAUNCHPAD */
+
+#endif /* SFUSAT_HWDEFS_H_ */
 
