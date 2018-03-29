2372ae3063d09a469e3c61491e0a61256139b62a
ultrawide <colinchan02@gmail.com>
2018-03-29 00:48:02 -0700

    MY FIRST COMMIT!

---
+++ SFUsat/sfu_error_logger.h
@@ -0,0 +1,43 @@
+/*
+ * sfu_error_logger.h
+ *
+ *  Created on: Mar 27, 2018
+ *      Author: Colin
+ *
+ *  See https://github.com/SFUSatClub/obc-firmware/issues/9
+ */
+
+#ifndef SFUSAT_ERROR_LOGGING_H_
+#define SFUSAT_ERROR_LOGGING_H_
+
+#include "FreeRTOS.h"
+#include "rtos_queue.h"
+
+#define QUEUE_LENGTH 20
+#define MESSAGE_LENGTH 128
+
+void vErrorLoggerInit(void *pvParameters);
+
+extern QueueHandle_t xErrorLoggingQueue;
+
+typedef enum {
+	error_code1,
+	error_code2,
+	error_code3,
+	error_code4,
+} Error_t;
+
+typedef enum {
+	error,
+	telemetry,
+	general_information
+} Message_t;
+
+typedef struct ErrorMessage {
+	uint32_t	rtc_epoch_time;
+	char 		description[MESSAGE_LENGTH];
+	Error_t 	error_code;
+	Message_t	message_type;
+} ErrorMessage_t;
+
+#endif // SFUSAT_ERROR_LOGGING_H_
