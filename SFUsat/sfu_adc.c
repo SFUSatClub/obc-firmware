44ea01413f5831ca8dc21a5b38062941e661aecf
richarthurs <arthursrich@gmail.com>
2018-06-26 18:07:34 -0400

    Fixed Merge Issue
    - also disabled some stuff in ADC file that was causing lots of warnings

--- SFUsat/sfu_adc.c
+++ SFUsat/sfu_adc.c
@@ -49,32 +49,32 @@ void adcUpdate(){
 	}
 }
 
-void vADCRead(void *pvParameters) {
-	// TODO: this task should start a conversion of group 1 (will read ADC channel 2 for starters)
-	// TODO: instead of waiting for the conversion to complete, add a semaphore from the ISR callback (adc1Group1Interrupt)
-	// this task would start conversion, suspend and wait for semaphore (xsemaphoretake). When the semaphore is raised (given) in adc1Group1Interrupt,
-	// it means the conversion is done. This task will take the semaphore back, then grab the data as we do now.
-	// With the data, create a new task and get the data into the task. - task notification, task param, queue, etc.
-	// In the new task, print out the data received.
-	// This is a lot of stuff, but it mimics what we will eventually need to do - call tasks to write data out to flash and save it.
-	// Chapter 6: https://www.freertos.org/Documentation/161204_Mastering_the_FreeRTOS_Real_Time_Kernel-A_Hands-On_Tutorial_Guide.pdf
-	// Note in this example we just run the function from the test. Normally we'd have the code in here, not call a far away function
-
-
-	while (1) {
-	    adcData_t adc_data[24]; // there are 24 channels
-	    char sendBuf[20];
-		// start conversion (it's inside test_adc)
-		// take a semaphore here
-
-		    adcStartConversion(adcREG1,adcGROUP1); // sample all channels on ADC1
-		    while((adcIsConversionComplete(adcREG1,adcGROUP1))==0); // wait for conversion to complete.
-		    adcGetData(adcREG1, adcGROUP1,&adc_data[0]); //
-
-		// if we get here, semaphore is taken, so we have data and can now print/send to other tasks
-
-		  snprintf(sendBuf, 20,"Current (mA): %d",adc_data[2].value);
-		  serialSendQ(sendBuf);
-		vTaskDelay(pdMS_TO_TICKS(2000)); // check every 2s
-	}
-}
+//void vADCRead(void *pvParameters) {
+//	// TODO: this task should start a conversion of group 1 (will read ADC channel 2 for starters)
+//	// TODO: instead of waiting for the conversion to complete, add a semaphore from the ISR callback (adc1Group1Interrupt)
+//	// this task would start conversion, suspend and wait for semaphore (xsemaphoretake). When the semaphore is raised (given) in adc1Group1Interrupt,
+//	// it means the conversion is done. This task will take the semaphore back, then grab the data as we do now.
+//	// With the data, create a new task and get the data into the task. - task notification, task param, queue, etc.
+//	// In the new task, print out the data received.
+//	// This is a lot of stuff, but it mimics what we will eventually need to do - call tasks to write data out to flash and save it.
+//	// Chapter 6: https://www.freertos.org/Documentation/161204_Mastering_the_FreeRTOS_Real_Time_Kernel-A_Hands-On_Tutorial_Guide.pdf
+//	// Note in this example we just run the function from the test. Normally we'd have the code in here, not call a far away function
+//
+//
+//	while (1) {
+//	    adcData_t adc_data[24]; // there are 24 channels
+//	    char sendBuf[20];
+//		// start conversion (it's inside test_adc)
+//		// take a semaphore here
+//
+//		    adcStartConversion(adcREG1,adcGROUP1); // sample all channels on ADC1
+//		    while((adcIsConversionComplete(adcREG1,adcGROUP1))==0); // wait for conversion to complete.
+//		    adcGetData(adcREG1, adcGROUP1,&adc_data[0]); //
+//
+//		// if we get here, semaphore is taken, so we have data and can now print/send to other tasks
+//
+//		  snprintf(sendBuf, 20,"Current (mA): %d",adc_data[2].value);
+//		  serialSendQ(sendBuf);
+//		vTaskDelay(pdMS_TO_TICKS(2000)); // check every 2s
+//	}
+//}
